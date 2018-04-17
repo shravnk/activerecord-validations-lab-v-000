221e15240673ec0070f319b58c8908a039862cca
Brendan O'Dea <brendan.ode4@gmail.com>
2018-04-17 19:43:35 +0000

    automatically backed up by learn

--- app/validate/clickbait_validator.rb
+++ app/validate/clickbait_validator.rb
@@ -2,7 +2,7 @@ class ClickbaitValidator < ActiveModel::EachValidator
   def validate_each(post, attribute, value)
     click_bait = ["Won't Believe", "Secret", "Top", "Guess"]
     unless click_bait.include?(value)
-      record.errors[attribute] << (options[:message] || "is not sufficiently clickbait-y")
+      post.errors[attribute] << (options[:message] || "is not sufficiently clickbait-y")
     end
 
   end
