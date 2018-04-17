98ade0d219911a793e5b11c0504a794f13847dd2
Brendan O'Dea <brendan.ode4@gmail.com>
2018-04-17 19:24:00 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,5 +3,5 @@ class Post < ActiveRecord::Base
   validates :title, presence: true
   validates :content, length: {minimum: 250}
   validates :summary, length: {maximum: 250}
-  validates :category, inclusion: {in: ["Fiction", "Non-Fiction"]}
+  validates :category, inclusion: ["Fiction", "Non-Fiction"]
 end
