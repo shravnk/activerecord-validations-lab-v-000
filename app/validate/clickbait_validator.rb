9f1a88728b72ce12febf867ac96cfdcc5e7effa7
Brendan O'Dea <brendan.ode4@gmail.com>
2018-04-17 19:42:32 +0000

    automatically backed up by learn

--- app/validate/clickbait_validator.rb
+++ app/validate/clickbait_validator.rb
@@ -1,4 +1,4 @@
-class ClickbaitValidator < ActiveRecord::EachValidator
+class ClickbaitValidator < ActiveModel::EachValidator
   def validate_each(post, attribute, value)
     click_bait = ["Won't Believe", "Secret", "Top", "Guess"]
     unless click_bait.include?(value)
